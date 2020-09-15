7a1810f9a1e33bf6d764708d59a47261bd7c5903
MiladEsp <32962296+MiladEsp@users.noreply.github.com>
2020-09-15 12:02:48 -0400

    Added the InsertionSort.cpp

---
+++ InsertionSort.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+void InsertionSort(std::vector<T> &vec, bool ascending)
+{
+    int i, j;
+    T key;
+
+    for (j = 1;j < vec.size();j++) {
+        key = vec[j];
+        i = j - 1;
+
+        if (ascending) {
+            while (i >= 0 && vec[i] > key) {
+                vec[i + 1] = vec[i];
+                i = i - 1;
+            }
+        }
+        else {
+            while (i >= 0 && vec[i] < key) {
+                vec[i + 1] = vec[i];
+                i = i - 1;
+            }
+        }
+        vec[i + 1] = key;
+    }
+}
+
+int main()
+{
+    std::vector<int> vec{ 5,2,4,7,1,3,2,6,-5,10,0 };
+
+    InsertionSort(vec, true);
+
+    for (auto item: vec) { std::cout << item << " "; }
+    std::cout << std::endl;
+
+    /////////////////////////
+    std::vector<double> vec1{ -2.3,0.5,1.6,-10.5,13.69 };
+
+    InsertionSort(vec1, true);
+
+    for (auto item: vec1) { std::cout << item << " "; }
+    std::cout << std::endl;
+
+    return 0;
+}
